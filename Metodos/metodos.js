6ee07e9ee9283089a694ed0960742b02666f2840
Viviana <vivioescobar@gmail.com>
2025-08-03 16:44:18 -0500

    funciones y metodos

---
+++ Metodos/metodos.js
@@ -0,0 +1,9 @@
+function imprimeMensage(message) {
+    console.log(message);
+}
+imprimeMensage("hola, soy muy buena programando");
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+var resultado = sumar(5, 10);
+console.log("Tu resultado es:  ".concat(resultado));
---
+++ Metodos/metodos.ts
@@ -0,0 +1,13 @@
+function imprimeMensage(message: string): void {
+  console.log(message);
+}
+
+imprimeMensage("hola, soy muy buena programando");
+
+function sumar(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+let resultado: number = sumar(5, 10);
+
+console.log(`Tu resultado es:  ${resultado}`);
