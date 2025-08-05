39fd875cf2206ac6bab9c2a3cb3273b2c8bbb9f4
Viviana <vivioescobar@gmail.com>
2025-08-05 16:58:44 -0500

    Clases

---
+++ Clases/implementacion.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var clase_1 = require("./clase");
+var persona = new clase_1.Persona("juan", 40, true);
+console.log(persona.saludar());
---
+++ Clases/implementacion.ts
@@ -0,0 +1,5 @@
+import { Persona } from "./clase";
+
+let persona = new Persona("juan", 40, true);
+
+console.log(persona.saludar());
