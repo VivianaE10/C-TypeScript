c3a276e72b5b805fe9c6889f66d08089ac105f12
Viviana <vivioescobar@gmail.com>
2025-08-13 16:08:26 -0500

    Organizacion de archivos typeScript para proyectos web

---
+++ SitioWeb/Src/main.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Hola desde el explorador");
+//esto es lo que typeScript le esta pidiendo a js por medio del html desplegar en la consola
---
+++ SitioWeb/Src/main.ts
@@ -0,0 +1,2 @@
+console.log("Hola desde el explorador");
+//esto es lo que typeScript le esta pidiendo a js por medio del html desplegar en la consola
